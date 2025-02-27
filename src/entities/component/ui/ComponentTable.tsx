ab104e8792e6f107719e0d27bddd708eb569f258
Roman <usankin.roma@gmail.com>
2025-02-27 23:49:55 +0400

    доработал построение таблицы компонентов

--- src/entities/component/ui/ComponentTable.tsx
+++ src/entities/component/ui/ComponentTable.tsx
@@ -1,19 +1,10 @@
-import {
-  Box,
-  CircularProgress,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow
-} from '@mui/material';
+import { Box, CircularProgress } from '@mui/material';
 import React from 'react';
 import { useComponent } from '../model';
 import { BACK_URL } from '../../../constants';
 import { NavLink } from 'react-router-dom';
 import ImagePreview from '../../../shared/ui/image-preview';
-import { ComponentDTO, EnumUnitMeasureTranslate } from '../model/product.dto';
+import { ComponentDTO } from '../model/product.dto';
 import TableCustom from '../../../shared/ui/table/ui/Table';
 
 const ComponentTable = () => {
@@ -24,18 +15,47 @@ const ComponentTable = () => {
       {
         title: 'Название',
         key: 'title',
-        getContent: (component) => (
-          <Box>{component.title}</Box>
-        )
+        getContent: (component: ComponentDTO) => (
+          <NavLink to={component.id}>{component.title}</NavLink>
+        ),
       },
       {
         title: 'Описание',
         key: 'description',
-        getContent: (component) => (
+        getContent: (component: ComponentDTO) => (
           <Box>{component.description}</Box>
-        )
-      }
-    ]
+        ),
+      },
+      {
+        title: 'Закупочная цена, р',
+        key: 'price',
+        getContent: (component: ComponentDTO) => <Box>{component.price}</Box>,
+      },
+      {
+        title: 'Изображение',
+        key: 'images',
+        getContent: (component: ComponentDTO) => (
+          <ImagePreview
+            imageUrl={`${BACK_URL}${component.images[0]}`}
+            altText={component.title}
+          />
+        ),
+      },
+      {
+        title: 'Ссылка на продавца',
+        key: 'images',
+        getContent: (component: ComponentDTO) => (
+          <NavLink to={component.seller_link} target="_blank">
+            Ссылка
+          </NavLink>
+        ),
+      },
+      {
+        title: '',
+        key: 'actions',
+        getContent: (component: ComponentDTO) => <Box>Delete Patch Watch</Box>,
+      },
+    ],
   };
 
   if (isComponentsListLoading) {
@@ -46,72 +66,7 @@ const ComponentTable = () => {
     return 'Данных не найдено';
   }
 
-  return (
-    <>
-      <TableCustom
-        tableConfig={tableConfig}
-        items={componentsList}
-      />
-      <Box overflow="hidden">
-        {/*<TableContainer sx={{ maxHeight: '100%' }}>*/}
-        {/*  <Table stickyHeader aria-label="sticky table">*/}
-        {/*    <TableHead>*/}
-        {/*      <TableRow>*/}
-        {/*        <TableCell>Название</TableCell>*/}
-        {/*        <TableCell>Описание</TableCell>*/}
-        {/*        <TableCell>Закупочная цена, р</TableCell>*/}
-        {/*        <TableCell>Изображение</TableCell>*/}
-        {/*        <TableCell>Ссылка на продавца</TableCell>*/}
-        {/*        <TableCell>Остаток</TableCell>*/}
-        {/*        <TableCell>Ед. измерения</TableCell>*/}
-        {/*        <TableCell></TableCell>*/}
-        {/*      </TableRow>*/}
-        {/*    </TableHead>*/}
-        {/*    <TableBody>*/}
-        {/*      {componentsList.map((componentsItem: ComponentDTO) => {*/}
-        {/*        return (*/}
-        {/*          <TableRow hover key={componentsItem.id}>*/}
-        {/*            <TableCell>*/}
-        {/*              <NavLink to={componentsItem.id}>*/}
-        {/*                {componentsItem.title}*/}
-        {/*              </NavLink>*/}
-        {/*            </TableCell>*/}
-        {/*            <TableCell>{componentsItem.description}</TableCell>*/}
-        {/*            <TableCell>{componentsItem.price}</TableCell>*/}
-        {/*            <TableCell>*/}
-        {/*              <ImagePreview*/}
-        {/*                imageUrl={`${BACK_URL}${componentsItem.images[0]}`}*/}
-        {/*                altText={componentsItem.title}*/}
-        {/*              />*/}
-        {/*            </TableCell>*/}
-        {/*            <TableCell>*/}
-        {/*              <NavLink to={componentsItem.seller_link} target="_blank">*/}
-        {/*                Ссылка*/}
-        {/*              </NavLink>*/}
-        {/*            </TableCell>*/}
-        {/*            <TableCell>{componentsItem.remainder}</TableCell>*/}
-        {/*            <TableCell>*/}
-        {/*              {EnumUnitMeasureTranslate[componentsItem.unit_measure]}*/}
-        {/*            </TableCell>*/}
-        {/*            <TableCell>Delete Patch Watch</TableCell>*/}
-        {/*          </TableRow>*/}
-        {/*        );*/}
-        {/*      })}*/}
-        {/*    </TableBody>*/}
-        {/*  </Table>*/}
-        {/*</TableContainer>*/}
-        {/*<TablePagination*/}
-        {/*  rowsPerPageOptions={[10, 25, 100]}*/}
-        {/*  component="div"*/}
-        {/*  count={rows.length}*/}
-        {/*  rowsPerPage={rowsPerPage}*/}
-        {/*  page={page}*/}
-        {/*  onPageChange={handleChangePage}*/}
-        {/*  onRowsPerPageChange={handleChangeRowsPerPage}*/}
-        {/*/>*/}
-      </Box>
-    </>
-  );
+  return <TableCustom tableConfig={tableConfig} items={componentsList} />;
 };
 
 export default ComponentTable;
