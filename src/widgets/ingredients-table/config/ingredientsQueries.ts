1d9bca088db8a336ede32b9959ad1224fd9fa5eb
roman <usankin.roma@gmail.com>
2026-01-24 14:30:45 +0400

    trade-wise-1: оптимизировал код получения ингридиентов в таблице

---
+++ src/widgets/ingredients-table/config/ingredientsQueries.ts
@@ -0,0 +1,19 @@
+export const INGREDIENTS_QUERIES = [
+  'page',
+  'pageSize',
+  'sort_by',
+  'direction',
+  'search',
+  'width_min',
+  'width_max',
+  'height_min',
+  'height_max',
+  'length_min',
+  'length_max',
+  'weight_min',
+  'weight_max',
+  'created_at_from',
+  'created_at_to',
+  'price_min',
+  'price_max'
+] as const
--- src/widgets/ingredients-table/model/useIngredientsTable.ts
+++ src/widgets/ingredients-table/model/useIngredientsTable.ts
@@ -2,14 +2,28 @@ import { useQuery } from '@tanstack/react-query'
 import { fetchIngredients } from '../../../entities/ingredient/api/ingredient.service.ts'
 import type { IIngredientsResponse, IUseIngredientsTableReturn } from './types.ts'
 import { useSearchParams } from 'react-router-dom'
+import { INGREDIENTS_QUERIES } from '../config/ingredientsQueries.ts'
+import { useMemo } from 'react'
+
+export const useSearchParamsFilters = () => {
+  const [searchParams] = useSearchParams()
+
+  return useMemo(() => {
+    const result: Record<string, string> = {}
+
+    INGREDIENTS_QUERIES.forEach(param => {
+      const value = searchParams.get(param)
+      if (value !== null) {
+        result[param] = value
+      }
+    })
+
+    return result
+  }, [searchParams])
+}
 
 export const useIngredientsTable = () : IUseIngredientsTableReturn => {
-  const [searchParams] = useSearchParams()
-  const page = searchParams.get('page') ?? undefined
-  const pageSize = searchParams.get('pageSize') ?? undefined
-  const sortBy = searchParams.get('sort_by') ?? undefined
-  const sortDirection = searchParams.get('direction') ?? undefined
-  const search = searchParams.get('search') ?? undefined
+  const ingredientsQueries = useSearchParamsFilters()
 
   const {
     isPending,
@@ -17,11 +31,9 @@ export const useIngredientsTable = () : IUseIngredientsTableReturn => {
     data,
     error
   } = useQuery({
-    queryKey: ['ingredients-table', page, pageSize, sortBy, sortDirection, search],
+    queryKey: ['ingredients-table', ingredientsQueries],
     queryFn: async(): Promise<IIngredientsResponse> => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      const response = await fetchIngredients({ page: page, per_page: pageSize, sort_by: sortBy, direction: sortDirection, search })
+      const response = await fetchIngredients(ingredientsQueries)
       return response.data
     }
   })
